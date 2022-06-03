86ece9f1032cf393d4d4bbae24670ed784c3d326
Abdul Wafi bin Haji Ismail <wafibismail@gmail.com>
2022-06-03 14:02:10 +0800

    12: Complete CubeSpawner3.cs

--- games/HelloWorld/CubeSpawner3.cs
+++ games/HelloWorld/CubeSpawner3.cs
@@ -27,5 +27,22 @@ public class CubeSpawner3 : MonoBehaviour {
 		gObj.transform.position = Random.insideUnitSphere;
 
 		gameObjectList.Add (gObj);
+
+		List<GameObject> removeList = new List<GameObject> ();
+
+		foreach (GameObject goTemp in gameObjectList) {
+			float scale = goTemp.transform.localScale.x;
+			scale *= scalingFactor;
+			goTemp.transform.localScale = Vector3.one * scale;
+
+			if (scale <= 0.1f) {
+				removeList.Add (goTemp);
+			}
+		}
+
+		foreach (GameObject goTemp in removeList) {
+			gameObjectList.Remove (goTemp);
+			Destroy (goTemp);
+		}
 	}
 }
